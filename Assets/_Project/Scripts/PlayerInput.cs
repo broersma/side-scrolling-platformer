d4ed160bebd1bd92c36f042199a0b3e49abeb13d
Eric Broersma <git@ericbroersma.nl>
2020-10-12 17:26:58 +0200

    Build very basic platformer

---
+++ Assets/_Project/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public CharacterController2D CharacterController;
+    public float MoveSpeed = 1f;
+
+    private float move;
+    private bool jump;
+    private bool crouch;
+
+    void Update()
+    {
+        // TODO crouch?
+        move = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        if ( Input.GetButtonDown("Jump") )
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        CharacterController.Move(move * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
